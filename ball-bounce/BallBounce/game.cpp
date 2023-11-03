df37bafdf528003b1dab34c278c1460f83a39195
Cole Busa <cole.busa@gmail.com>
2023-11-02 20:31:44 -0400

    Added basic sprite rendering

--- ball-bounce/BallBounce/game.cpp
+++ ball-bounce/BallBounce/game.cpp
@@ -1,4 +1,11 @@
 #include "game.h"
+#include "graphics/resource_manager.h"
+#include "graphics/sprite_renderer.h"
+
+
+// Game-related State data
+SpriteRenderer* Renderer;
+
 
 Game::Game(unsigned int width, unsigned int height)
     : State(GAME_ACTIVE), Keys(), Width(width), Height(height)
@@ -8,12 +15,22 @@ Game::Game(unsigned int width, unsigned int height)
 
 Game::~Game()
 {
-
+    delete Renderer;
 }
 
 void Game::Init()
 {
-
+    // load shaders
+    ResourceManager::LoadShader("graphics/sprite.vs", "graphics/sprite.fs", nullptr, "sprite");
+    // configure shaders
+    glm::mat4 projection = glm::ortho(0.0f, static_cast<float>(this->Width),
+        static_cast<float>(this->Height), 0.0f, -1.0f, 1.0f);
+    ResourceManager::GetShader("sprite").Use().SetInteger("image", 0);
+    ResourceManager::GetShader("sprite").SetMatrix4("projection", projection);
+    // set render-specific controls
+    Renderer = new SpriteRenderer(ResourceManager::GetShader("sprite"));
+    // load textures
+    ResourceManager::LoadTexture("graphics/awesomeface.png", true, "face");
 }
 
 void Game::Update(float dt)
@@ -28,5 +45,5 @@ void Game::ProcessInput(float dt)
 
 void Game::Render()
 {
-
+    Renderer->DrawSprite(ResourceManager::GetTexture("face"), glm::vec2(200.0f, 200.0f), glm::vec2(300.0f, 400.0f), 45.0f, glm::vec3(0.0f, 1.0f, 0.0f));
 }
